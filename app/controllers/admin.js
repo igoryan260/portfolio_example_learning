4bd5bd6d8db64a9f12b3f2abe09caf18cc788ecc
Igor Silva <igor.silva662@etec.sp.gov.br>
2021-12-25 12:12:00 -0300

    Mudança de diretório, novas funcionalidades e página de cadastro e login feito.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,3 +1,5 @@
+var md5 = require("md5")
+
 module.exports.login = (req, res) => {
 
     const { MongoClient } = require("mongodb");
@@ -13,24 +15,22 @@ module.exports.login = (req, res) => {
             let database = client.db("portfolio_example")
             let colecao = database.collection("admin")
 
+            //para a segurança do sistema, vamos criptografar a senha e pesquisar a senha já criptografada
+            req.body.senhaLogin = md5(req.body.senhaLogin)
+
             // conexão estabelecida
             // a variavel abaixo se transformará num array contendo as informações retiradas do banco de dados
-            const result = await colecao.find({ administrador: req.body.nomeAdm }).toArray()
+            const result = await colecao.find({ administrador: req.body.nomeLogin, senha: { $eq: req.body.senhaLogin } }).toArray()
 
 
             //o codigo abaixo irá verificar o resultado que é um array, e logo pegará seu primeiro índice e comparar com a informação digitada no formulário
             if (result[0]) {
-
-                if (result[0].administrador === req.body.nomeAdm) {
-
+                if (result[0].administrador === req.body.nomeLogin) {
                     res.send("Você existe na base de dados: " + result[0].administrador)
                         //console.log("Você existe na base de dados")
-
                 } else {
-
                     //console.log("Você não existe na base de dados")
                     res.send("Você não existe na base de dados")
-
                 }
             } else {
                 //caso não tenha retornado nada na variável "result", a aplicação retornará na página 'admin' com o erro
@@ -51,4 +51,43 @@ module.exports.login = (req, res) => {
     //console.log(req.body.nomeAdm)
     //res.send("Página de administrador: " + req.body.nomeAdm)
 
+}
+
+module.exports.register = (req, res) => {
+
+    //criando objeto do mongoclient
+    const { MongoClient } = require("mongodb")
+
+    //uri da base de dados
+    const uri = "mongodb://localhost:27017"
+
+    //instanciando o objeto mongo
+    const client = new MongoClient(uri)
+
+    //o código abaixo utiliza a tecnologia MD5 para criptografar as senhas dos usuarios
+    req.body.senhaRegister = md5(req.body.senhaRegister)
+
+
+    async function run() {
+        try {
+            await client.connect()
+            console.log("Conexão iniciada")
+
+            const database = client.db("portfolio_example")
+            const collection = database.collection("admin")
+
+            const doc = { administrador: req.body.adminRegister, senha: req.body.senhaRegister }
+
+            await collection.insertOne(doc)
+            console.log("Documento inserido")
+
+        } finally {
+            //encerrando conexão após o uso para a segurança do sistema
+            await client.close()
+            res.render("admin.ejs")
+            console.log("Conexão encerrada")
+        }
+    }
+
+    run().catch(console.dir)
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,9 +1,13 @@
 module.exports = (application) => {
-    /*application.get("/admin", (req, res) => {
-        application.app.controllers.admin.login(req, res)
-    })*/
+    application.get("/admin", (req, res) => {
+        res.render("admin.ejs")
+    })
 
-    application.post('/admin', (req, res) => {
+    application.post('/admin/login', (req, res) => {
         application.app.controllers.admin.login(req, res)
+    })
+
+    application.post('/admin/register', (req, res) => {
+        application.app.controllers.admin.register(req, res)
     })
 }
