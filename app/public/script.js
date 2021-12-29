e417ba53acbed0b97cc034cc164d6c5dfd33abc1
user.name <othon.santos.silva@gmail.com>
2021-12-28 22:46:11 -0300

    Atualização do submenu

---
+++ app/public/script.js
@@ -0,0 +1,27 @@
+function mostrarContatos(){
+    document.getElementById('subMenu-contatos').className = 'mostrar-subMenu-contatos' 
+}
+
+function esconderContatos(){
+    document.getElementById('subMenu-contatos').className = 'subMenu-contatos' 
+}
+
+function mostrarConfiguracoes(){
+    document.getElementById('subMenu-configuracoes').className = 'mostrar-subMenu-configuracoes'
+}
+
+function esconderConfiguracoes(){
+    document.getElementById('subMenu-configuracoes').className = 'subMenu-configuracoes' 
+}
+
+function mostrarHamburguer(){  
+    document.getElementById('subMenu-hamburguer').className = 'mostrar-subMenu-hamburguer'
+}
+
+function esconderHamburguer(){
+    document.getElementById('subMenu-hamburguer').className = 'subMenu-hamburguer' 
+}
+
+
+
+
